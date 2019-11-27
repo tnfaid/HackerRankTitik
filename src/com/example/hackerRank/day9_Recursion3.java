cf819bb058c73536b4f4b012045d52aafef22ec5
titik <titik.nurfaidah@1rstwap.com>
2019-11-27 17:44:34 +0700

    day9 Recursion3

---
+++ src/com/example/hackerRank/day9_Recursion3.java
@@ -0,0 +1,31 @@
+package com.example.hackerRank;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class day9_Recursion3 {
+    static int factorial(int n){
+//        this is my code
+        return (n==1) ? 1 : n*factorial(n-1);
+//        end of my code
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter((System.getenv("OUTPUT_PATH"))));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+        scanner.close();
+    }
+}
