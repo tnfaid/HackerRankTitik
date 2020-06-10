7fc829114f0c76312e11ee3235fb823c31bd83d4
Titik Nurfaidah <titik.nurfaidah@1rstwap.com>
2020-06-10 11:44:14 +0700

    sort and merged done

--- src/com/example/hackerRank/BubleSort.java
+++ src/com/example/hackerRank/BubleSort.java
@@ -2,111 +2,48 @@ package com.example.hackerRank;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BubleSort
 {
 
-    public static void sort( String[] list, int count )
+    public static String[][] sort(String[][] list, int count)
     // A very simple, stupid Bubble Sort
     // list[] contains the Strings to be sorted
     // count tells how many Strings are in the array
     {
-        for (int pass = 0; pass < count; pass = pass+1)
+        String temp[][] = new String[3][4];
+        for (int pass = 0; pass < count; pass++)
         {
-            for (int p = 0; p < count-1; p = p + 1)
+            for (int p = 0; p < count-1; p++)
             {
-                if (list[p].compareTo(list[p+1])>0)
+                if (list[p][0].compareTo(list[p+1][0])>0)
                 {
-                    String temp = list[p];
-                    list[p] = list[p+1];
-                    list[p+1] = temp;
+                    temp[p][0] = list[p][0];
+                    temp[p][1] = list[p][1];
+                    temp[p][2] = list[p][2];
+                    temp[p][3] = list[p][3];
+
+                    list[p][0] = list[p+1][0];
+                    list[p][1] = list[p+1][1];
+                    list[p][2] = list[p+1][2];
+                    list[p][3] = list[p+1][3];
+
+                    list[p+1][0] = temp[p][0];
+                    list[p+1][1] = temp[p][1];
+                    list[p+1][2] = temp[p][2];
+                    list[p+1][3] = temp[p][3];
                 }
             }
         }
-    }
-
-
-    public static String BubleSort()
-    {
-        String[] list = new String[1000];
-        int count = 0;
-        String line1;
-        String line2;
-        ArrayList<String> Prodi = new ArrayList<>();
-
-        try
-        {
-            // Baca File
-            BufferedReader mahasiswa = new BufferedReader(new FileReader("/home/titik/Downloads/mahasiswa.txt"));
-            BufferedReader jurusan = new BufferedReader(new FileReader("/home/titik/Downloads/jurusan.txt"));
-
-            while (mahasiswa.ready())
-            {
-                String item = mahasiswa.readLine();
-                list[count] = item;
-                count = count + 1;
-            }
-
-            while ((line1 = jurusan.readLine()) != null)
-            {
-                String[] arrOfStr = line1.split(",");
-
-                for( int i = 0; i <arrOfStr.length ; i++ )
-                {
-                    if(i==1) Prodi.add(arrOfStr[i]);
-                }
-            }
-
-            String[][] hasil2 = new String[3][4];
-            int dataRowMahasiswa = 0;
-            while((line2 = mahasiswa.readLine()) != null){
-
-                String[] arrOfStr = line2.split(",");
-                for( int i = 0; i <arrOfStr.length ; i++ )
-                {
-                    if(i==3) {
-                        if(arrOfStr[i].equals("1")){
-                            arrOfStr[3] = Prodi.get(0);
-                        }
-                        if(arrOfStr[i].equals("2")){
-                            arrOfStr[3]  = Prodi.get(1);
-                        }
-                        if(arrOfStr[i].equals("3")){
-                            arrOfStr[3]  = Prodi.get(2);
-                        }
-                        if(arrOfStr[i].equals("4")){
-                            arrOfStr[3]  = Prodi.get(3);
-                        }
-                    }
-                    hasil2[dataRowMahasiswa][i] = arrOfStr[i];
-                }
-                dataRowMahasiswa++;
-            }
-
-            System.out.println(hasil2);
-
-            sort(list,count);
-            mahasiswa.close();
-
-            // output sorting
-            for (int x = 0; x < count; x = x+1)
-            {
-                list[x] = list[x].replaceAll(",","\t");
-                System.out.println(list[x]);
-            }
-
-            System.out.close();
-        }
-        catch (IOException ex)
-        {
-            System.out.println(ex.toString());
-        }
-        return null;
+        return list;
     }
 
     public static void merged()
     {
         try {
+            String[] list = new String[1000];
+            int count = 0;
             String line1;
             String line2;
             ArrayList<String> Prodi = new ArrayList<>();
@@ -115,9 +52,6 @@ public class BubleSort
             BufferedReader jurusan = new BufferedReader(new FileReader("/home/titik/Downloads/jurusan.txt"));
             BufferedReader mahasiswa = new BufferedReader(new FileReader("/home/titik/Downloads/mahasiswa.txt"));
 
-            System.out.println("hai dari mahasiswa " + mahasiswa);
-//            String mahasiswa = new BubleSort("/home/titik/Downloads/mahasiswa.txt");
-//            new BubleSort("/home/titik/Downloads/mahasiswa.txt");
 
             while ((line1 = jurusan.readLine()) != null)
             {
@@ -131,9 +65,10 @@ public class BubleSort
 
             String[][] hasil2 = new String[3][4];
             int dataRowMahasiswa = 0;
-            while((line2 = mahasiswa.readLine()) != null){
 
+            while((line2 = mahasiswa.readLine())!= null){
                 String[] arrOfStr = line2.split(",");
+
                 for( int i = 0; i <arrOfStr.length ; i++ )
                 {
                     if(i==3) {
@@ -156,17 +91,16 @@ public class BubleSort
             }
 
 
+            hasil2 = sort(hasil2, 3);
+            System.out.println("hasil");
             for( int i = 0; i <3 ; i++ )
             {
                 for( int j = 0; j < 4; j++ )
                 {
                     System.out.print(hasil2[i][j] + "\t");
-                    String temp1 = hasil2[i][j];
                 }
                 System.out.println();
             }
-
-
             // list[] contains the Strings to be sorted
             // count tells how many Strings are in the array
 
@@ -181,7 +115,6 @@ public class BubleSort
     {
         System.out.println("\n ****************** \njadi ini test2 sorting \n");
 //        System.out.println("BubleSort");
-//        BubleSort();
 //        BubleSort();
 
         System.out.println("ini merged");
